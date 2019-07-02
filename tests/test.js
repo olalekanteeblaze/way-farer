4a115ac34ddb369daaf37e5cef8896d5672f40c7
olalekanteeblaze <olalekanteeblaze@gmail.com>
2019-07-02 19:04:22 +0100

    Added a readme badge

---
+++ tests/test.js
@@ -0,0 +1,43 @@
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();
+
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Books', () => {
+    beforeEach((done) => { //Before each test we empty the database
+        Book.remove({}, (err) => { 
+           done();           
+        });        
+    });
+/*
+  * Test the /GET route
+  */
+  describe('/GET book', () => {
+      it('it should GET all the books', (done) => {
+        chai.request(server)
+            .get('/book')
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('array');
+                  res.body.length.should.be.eql(0);
+              done();
+            });
+      });
+  });
+
+});
+describe('/GET trip', () => {
+    it('should GET User trip', (done) => {
+        chai.request(server)
+        .get('/trip')
+        .end((err,res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            res.body.length.should.be.eql(0)
+        })
+    })
+})
